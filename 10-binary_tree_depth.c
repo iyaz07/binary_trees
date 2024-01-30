c1d711e3a92de172d42f2164d655846040029099
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-01-30 15:55:28 +0000

    task 0-13

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,7 +8,7 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-size_t depth;
+size_t depth = 0;
 
 if (tree == NULL)
 {
@@ -17,8 +17,6 @@ if (tree == NULL)
 
 if (tree->parent != NULL)
 { depth = 1 + binary_tree_depth(tree->parent); }
-else
-{ depth = 0; }
 
 return (depth);
 }
